e182e03b627592f035d9137c115bfae2f470fe94
Danil-Gafarov <officialdanilgafarov@gmail.com>
2021-10-31 17:21:49 +0300

    pylint and black improvements

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,6 +1,3 @@
-import typing as tp
-
-
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -47,13 +44,3 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
         else:
             plaintext += char
     return plaintext
-
-'''
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
-'''
