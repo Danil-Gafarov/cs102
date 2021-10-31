9b0ccc412638ad04b9d423db7f484538574f92b5
Danil-Gafarov <officialdanilgafarov@gmail.com>
2021-10-31 17:04:22 +0300

    pylint improvements

--- homework00/hello.py
+++ homework00/hello.py
@@ -2,6 +2,9 @@ def get_greeting(name: str) -> str:
     return "Hello, " + name + "!"
 
 
+"""
+docstring for pylint 
+"""
 if __name__ == "__main__":
     MESSAGE = get_greeting("World")
     print(MESSAGE)
