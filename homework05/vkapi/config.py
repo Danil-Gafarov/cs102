b281b6dc61442aaefc5ad3ee3ae3131220e2740d
Danil-Gafarov <officialdanilgafarov@gmail.com>
2022-03-28 23:02:15 +0300

    Improved solution

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "53648182718e80672e4acf8689ff77792d3cefc927bdf87a8964a7a464f80f43bd6a41a248608e6f27179",
     "version": "5.126",
 }
